1842240a4a43bcecd0bcd55bacb759430a114751
Shitalbhalgat <shitalbhalgat@gmail.com>
2025-05-08 21:13:40 +0530

    Operators

---
+++ basic/Operators.py
@@ -0,0 +1,59 @@
+#Addition operator
+a = 20
+b = 10
+print ("a + b : ", a + b)
+print ("a - b : ", a - b)
+print ("a * b : ", a * b)
+print ("a / b : ", a / b)
+print ("a % b : ", a % b)
+print ("a ** b : ", a ** b)
+print ("a // b : ", a // b)
+
+#comparsion operators
+a = 4
+b = 5
+print ("a == b : ", a == b)
+print ("a != b : ", a != b)
+print ("a > b : ", a > b)
+print ("a < b : ", a < b)
+print ("a >= b : ", a >= b)
+print ("a <= b : ", a <= b)
+
+
+# Assignment Operator
+a=10
+a += 5
+print ("a += 5 : ", a)
+a -= 5
+print ("a -= 5 : ", a)
+a *= 5
+print ("a *= 5 : ", a)
+a /= 5
+print ("a /= 5 : ",a)
+a %= 3
+print ("a %= 3 : ", a)
+a **= 2
+print ("a **= 2 : ", a)
+a //= 3
+print ("a //= 3 : ", a)
+
+#Logical Operator
+a = True
+b = False
+print(a and b)
+print(a or b)
+print(not a)
+
+#identity operator
+a=5
+b=5
+print("a is b",(a is b))
+print("a is not  b",(a is not b))
+a=[1,2,3]
+b=[1,2,3]
+print("a is b",(a is b))
+print("a is not b",(a is not b))
+a=(1,2,3)
+b=(1,2,3)
+print("a is b",(a is b))
+print("a is not b",(a is not b))
